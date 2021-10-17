74933a176309d856a31d99515f259178432abd6a
Shriram Bhagwatwar <42444632+sabhagwatwar1@users.noreply.github.com>
2021-10-17 23:14:40 +0530

    Add files via upload

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,75 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define n 20
-class Queue
+struct node
 {
-    int *arr;
-    int front;
-    int back;
-     
-    public:
-    Queue()
+    int data;
+    node* left;
+    node* right;
+    node(int val)
     {
-        arr=new int[n];
-        front=-1;
-        back=-1;
-    }
-    void push(int x)
-    {
-        if(back==n-1)
-        {
-            cout<<"queue overflow";
-            return;
-        }
-        back++;
-        arr[back]=x;
-
-        if(front==-1)
-        {
-            front++;
-        }
-    }
-    void pop()
-    {
-        if(front==-1 || front>back)
-        {
-            cout<<"No element";
-            return;
-        }
-        front++;
-    }
-    int peek()
-    {
-        if(front==-1 || front>back)
-        {
-            cout<<"No element";
-            return -1;
-        } 
-        return arr[front];
-    }
-    bool empty()
-    {
-        if(front==-1 || front>back)
-        {
-            return true;
-        }
-        return false;
-
+        data=val;
+        left=NULL;
+        right=NULL;
     }
 };
+
+void LOT(node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node* node=q.front();
+        q.pop();
+        if(node!=NULL)
+        {
+            cout<<node->data;
+            if(node->left)
+            {
+                q.push(node->left);
+            }
+            if(node->right)
+            {
+                q.push(node->right);
+            }
+        }
+    }
+    
+}
+void Preorder(node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    cout<<root->data;
+    Preorder(root->left);
+    Preorder(root->right);
+
+}
 int main()
 {
-    Queue s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    cout<<s.empty()<<endl;
-    cout<<s.peek()<<endl;
-    s.pop();
-    s.pop();
-    cout<<s.peek()<<endl;
-    s.pop();
-    cout<<s.peek()<<endl;
-    cout<<s.empty();
-
+    node* root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    root->right->left=new node(4);
+    root->left->right=new node(5);
+    Preorder(root);
+    cout<<endl;
+    LOT(root);
+    cout<<endl;
+    Preorder(root);
 }
